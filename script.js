02569d0250011228d726daa385eb348d1d204daf
JBisquick <felipebirch@gmail.com>
2023-08-31 16:42:35 -0700

    Add equal button eventlistener

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-const numbers = document.querySelectorAll('.number');
+const higherDisplay = document.querySelector('#higher');
 const lowerDisplay = document.querySelector('#lower');
-let firstNumber = 12;
-let secondNumber = 6;
-let operator = '/';
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('#equals');
+let firstNumber = 0;
+let secondNumber = 0;
+let operatorValue = '';
 let displayValue = '';
+
+equals.addEventListener('click', () => {
+  secondNumber = parseFloat(displayValue);
+  lowerDisplay.textContent = operate(firstNumber, secondNumber, operatorValue);
+  higherDisplay.textContent += ` ${secondNumber} =`
+}); 
+
+numbers.forEach((number) => {
+  number.addEventListener('click', () => {
+    // Prevents display number from starting with a 0
+    if (lowerDisplay.textContent === '0' && number.textContent === '0') {
+      lowerDisplay.textContent = '0';
+    } else {
+      displayValue += number.textContent;
+      lowerDisplay.textContent = displayValue;
+    }
+  });
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener('click', () => {
+    operatorValue = operator.textContent;
+    higherDisplay.textContent = `${displayValue} ${operatorValue}`;
+    firstNumber = parseFloat(displayValue);
+    displayValue = ''
+  });
+});
 
 function add(a, b) {
   return a + b;
@@ -33,14 +63,3 @@ function operate(a, b, operator) {
       return divide(a, b);
   }
 };
-
-numbers.forEach((number) => {
-  number.addEventListener('click', () => {
-    if (lowerDisplay.textContent === '0' && number.textContent === '0') {
-      lowerDisplay.textContent = '0';
-    } else {
-      displayValue += number.textContent;
-      lowerDisplay.textContent = displayValue;
-    }
-  });
-});
