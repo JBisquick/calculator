8bf799e6935c7b4a5e65708edc0d3dba6effe72e
JBisquick <felipebirch@gmail.com>
2023-09-01 15:28:31 -0700

    Round to 3rd decimal place

--- script.js
+++ script.js
@@ -28,19 +28,19 @@ operators.forEach((operator) => {
 });
 
 function add(a, b) {
-  return a + b;
+  return Math.round((a + b) * 1000) / 1000;
 };
 
 function subtract(a, b) {
-  return a - b;
+  return Math.round((a - b) * 1000) / 1000;
 };
 
 function multiply(a, b) {
-  return a * b;
+  return Math.round((a * b) * 1000) / 1000;
 };
 
 function divide(a, b) {
-  return a / b;
+  return Math.round((a / b) * 1000) / 1000;
 };
 
 function operate(a, b, operator) {
